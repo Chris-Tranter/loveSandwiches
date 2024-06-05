e937ff46fa535de8eca5530c5752f33ba64e8078
Chris Tranter <135023953+Chris-Tranter@users.noreply.github.com>
2024-06-05 22:01:39 +0100

    added error message for when characters are used

--- run.py
+++ run.py
@@ -31,6 +31,7 @@ def get_sales_data():
     sales_data = data_str.split(",")
     validate_data(sales_data)
 
+
 def validate_data(values):
     """
     Inside the try, converts all string values into integers.
@@ -38,9 +39,13 @@ def validate_data(values):
     or if there aren't exactly 6 values.
     """
     try:
+        [int(value) for value in values]
         if len(values) != 6:
-            raise ValueError(f"Exactly 6 values required, you provided {len(values)}")
+            raise ValueError(
+                f"Exactly 6 values required, you provided {len(values)}"
+            )
     except ValueError as e:
-        print(f"Invalid data: {e}, please try again.\n")
+        print(f"Invalid data: Use numbers not letters.{e}\n")
+
 
 get_sales_data()
