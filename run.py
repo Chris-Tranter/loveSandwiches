76a7a38266c8beb4a2bb38d49aa16fa6667503b4
Chris Tranter <135023953+Chris-Tranter@users.noreply.github.com>
2024-06-05 23:06:02 +0100

    function to pull the data from last row in surplus sheet

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import gspread
 from google.oauth2.service_account import Credentials
+from pprint import pprint
 
 # All constant variables in python have capital letters!
 # SET the scope and list the API's available
@@ -12,7 +13,7 @@ SCOPE = [
 
 # Pulling from the imported gspread we retrieve the service account
 
-CREDS = Credentials.from_service_account_file('creds.JSON')
+CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('loveSandwiches')
@@ -32,7 +33,6 @@ def get_sales_data():
         print("Example: 10,20,30,40,50,60\n")
 
         data_str = input("Enter your data here: ")
-
         sales_data = data_str.split(",")
 
         if validate_data(sales_data):
@@ -40,7 +40,6 @@ def get_sales_data():
             break
 
     return sales_data
-
 
 def validate_data(values):
     """
@@ -60,7 +59,6 @@ def validate_data(values):
 
     return True
 
-
 def update_sales_worksheet(data):
     """
     Update sales worksheet, add new row with the list data provided
@@ -70,7 +68,33 @@ def update_sales_worksheet(data):
     sales_worksheet.append_row(data)
     print("Sales worksheet updated successfully.\n")
 
+# surplus data calculator parsed with sales data list 
 
-data = get_sales_data()
-sales_data = [int(num) for num in data]
-update_sales_worksheet(sales_data)
+def calculate_surplus_data(sales_row):
+    """
+    Compares the sales and stock then calculates the surplus data.
+    - Positive surplus indicates waste
+    - Negative surplus indicates extra sandwiches made.
+    """
+    print("Calculating surplus data...\n")
+    #                           GET ALL VALUES is another method from the gspread library
+    stock = SHEET.worksheet("stock").get_all_values()
+    #                 -1       pulls the last list from the stock sheet
+    stock_row = stock[-1]
+    print(stock_row)
+
+# wrap main function calls in MAIN FUNCTION
+
+def main():
+    """
+    main functions for the program
+    """
+    data = get_sales_data()
+    sales_data = [int(num) for num in data]
+    update_sales_worksheet(sales_data)
+    calculate_surplus_data(sales_data)
+
+# displays text for the functions purpose
+
+print('Welcome to Data Automation for loveSandwiches')
+main()
